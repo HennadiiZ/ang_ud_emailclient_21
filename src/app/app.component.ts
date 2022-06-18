4a4aaf2f44bedf31fb5339262fe343546135f700
Hennadii <82006228+HennadiiZ@users.noreply.github.com>
2022-06-18 15:45:31 +0300

    Inbox Module Design / (ng g m Inbox --routing) / (ng g c inbox/Home)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -91,6 +91,6 @@ export class AppComponent implements OnInit{
   // 311. Building the Sign In Flow
   // 312. Sign In Authentication
   // 313. Showing Authentication Errors
-  // 
-
+  // 314. Inbox Module Design / (ng g m Inbox --routing) / (ng g c inbox/Home)
+  //
 }
